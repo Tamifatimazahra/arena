df683e43d137e2cf1d063f4938d4dd360be29a17
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-03-03 13:34:36 +0100

    Initialisation et Structure Modulaire

---
+++ Desktop/arenasync/src/main.js
@@ -0,0 +1,7 @@
+// import { tournamentData } from "./data/tournamentDB.js";
+// console.log(tournamentData);
+
+console.log("HELLO");
+import { tournamentData } from "./data/tournamentDB.js";
+console.log(tournamentData);
+
